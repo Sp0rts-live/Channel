3093b77536b204ad1357b286f3f02278a961629e
Sports <131376820+Sp0rts-live@users.noreply.github.com>
2025-03-09 13:34:01 +0530

    Update and rename telegram.js to common.js

--- assets/telegram.js
+++
@@ -1,9 +0,0 @@
-function loadTelegramButton() {
-    fetch('/includes/telegram.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("telegram-container").innerHTML = data;
-        });
-}
-
-document.addEventListener("DOMContentLoaded", loadTelegramButton);
