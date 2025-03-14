762735ad6ad213e15ad8ae1197245d336dc0392b
Sports <131376820+Sp0rts-live@users.noreply.github.com>
2025-03-14 19:54:12 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
-// Toggle Sidebar
 function toggleMenu() {
-    let sidebar = document.getElementById("sidebar");
-    sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
+    var sidebar = document.getElementById("sidebar");
+    var overlay = document.getElementById("overlay");
+    
+    if (sidebar.classList.contains("active")) {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+    } else {
+        sidebar.classList.add("active");
+        overlay.classList.add("active");
+    }
 }
 
+// Disable Right Click & Inspect
+document.addEventListener("contextmenu", event => event.preventDefault());
 
+document.addEventListener("keydown", event => {
+    if (event.ctrlKey && (event.key === 'u' || event.key === 's' || event.key === 'i' || event.key === 'j' || event.key === 'c') || event.key === 'F12') {
+        event.preventDefault();
+    }
+});
