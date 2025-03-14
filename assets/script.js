68a2f534c57895ae29b825e8873fca98bc740190
Sports <131376820+Sp0rts-live@users.noreply.github.com>
2025-03-14 14:26:37 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,4 @@ function toggleMenu() {
     sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
 }
 
-// Disable Right Click & Inspect
-document.addEventListener("contextmenu", event => event.preventDefault());
 
-document.addEventListener("keydown", event => {
-    if (event.ctrlKey && (event.key === 'u' || event.key === 's' || event.key === 'i' || event.key === 'j' || event.key === 'c') || event.key === 'F12') {
-        event.preventDefault();
-    }
-});
